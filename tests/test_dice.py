efeeac67067e9e3ec8a563e3952f7915def33f32
yannis bouhamou <yannis.bouhamou@hotmail.fr>
2023-10-11 10:01:10 +0200

    improve test

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -5,5 +5,8 @@ def test_dice_roll():
     result = Dice().roll()
     assert result > 0 
     assert result < 7 
+
+
+def test_dice_roll_int(): 
+    result = Dice().roll()
     assert type(result) == int  
-    
